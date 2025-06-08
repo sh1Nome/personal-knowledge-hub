4ba424531dcfd77ca9fa2e554cdbf7faf805e794
sh1nome <shinodayo1024@gmail.com>
2025-06-08 15:24:32 +0900

    Docker関連の設定を削除し、zshのインストールタスクを追加して初期化スクリプトを更新

--- tool/linux/init-dev-env.sh
+++ tool/linux/init-dev-env.sh
@@ -14,5 +14,8 @@ read GIT_USER_NAME
 echo "Git user.email を入力してください: "
 read GIT_USER_EMAIL
 
+# SUDOを実行したユーザーのホームディレクトリを取得
+SUDO_USER_HOME=$(eval echo ~$SUDO_USER)
+
 # Ansible Playbook の実行
-ansible-playbook -i localhost, ./init-dev-env.yml --extra-vars "git_user_name=$GIT_USER_NAME git_user_email=$GIT_USER_EMAIL"
+ansible-playbook -i localhost, ./init-dev-env.yml --extra-vars "git_user_name=$GIT_USER_NAME git_user_email=$GIT_USER_EMAIL sudo_user=$SUDO_USER sudo_user_home=$SUDO_USER_HOME"
